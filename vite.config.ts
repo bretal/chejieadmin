8eee76cbb627d274429c32f71ac8b11df368323b
lhr <lvhaorankk@gmail.com>
2026-05-26 17:18:23 +0800

    feat:配置cdn获得全球加速体验

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
   plugins: [react()],
+  base: env.VITE_BASE_URL || '/',
   server: {
     port: 3000,
     proxy: {
@@ -26,4 +30,5 @@ export default defineConfig({
       },
     },
   },
+  }
 })
